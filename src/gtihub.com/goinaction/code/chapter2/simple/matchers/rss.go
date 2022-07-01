37025e1ee89ea029177c7adbc6a8fbcf44476858
lulu <411895715@qq.com>
2022-07-01 11:23:26 +0800

    第二单元代码提交

--- src/gtihub.com/goinaction/code/chapter2/simple/matchers/rss.go
+++ src/gtihub.com/goinaction/code/chapter2/simple/matchers/rss.go
@@ -1,1 +1,140 @@
 package matchers
+
+import (
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"goinaction/src/gtihub.com/goinaction/code/chapter2/simple/search"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+type (
+	// item 根据item 字段的标签，将定义的字段
+	// 与rss 文档的字段关联起来
+	item struct {
+		XMLName     xml.Name `xml:"item"`
+		PubDate     string   `xml:"pubDate"`
+		Title       string   `xml:"title"`
+		Description string   `xml:"description"`
+		Link        string   `xml:"link"`
+		GUID        string   `xml:"guid"`
+		GeoRssPoint string   `xml:"point"`
+	}
+
+	// image 根据image 字段的标签，将定义的字段
+	// 与rss 文档的字段关联起来
+	image struct {
+		XMLName xml.Name `xml:"image"`
+		URL     string   `xml:"url"`
+		Title   string   `xml:"title"`
+		Link    string   `xml:"link"`
+	}
+
+	// channel 根据channel 字段的标签，将定义的字段
+	// 与rss 文档的字段关联起来
+	channel struct {
+		XMLName        xml.Name `xml:"channel"`
+		Title          string   `xml:"title"`
+		Description    string   `xml:"description"`
+		Link           string   `xml:"link"`
+		PubDate        string   `xml:"pubDate"`
+		LastBuildDate  string   `xml:"lastBuildDate"`
+		TTL            string   `xml:"ttl"`
+		Language       string   `xml:"language"`
+		ManagingEditor string   `xml:"managingEditor"`
+		WebMaster      string   `xml:"webMaster"`
+		Image          image    `xml:"image"`
+		Item           []item   `xml:"item"`
+	}
+
+	// ssDocument 定义了与rss 文档关联的字段
+	rssDocument struct {
+		XMLName xml.Name `xml:"rss"`
+		Channel channel  `xml:"channel"`
+	}
+)
+
+// rssMatcher 实现了Matcher 接口
+type rssMatcher struct {
+}
+
+func init() {
+	var matcher rssMatcher
+	search.Register("rss", matcher)
+}
+
+// Search 在文档中查找特定的搜索项
+func (m rssMatcher) Search(feed *search.Feed, searchTerm string) ([]*search.Result, error) {
+	var results []*search.Result
+
+	log.Printf("Search Feed Type[%s] Site[%s] For URI[%s]\n", feed.Type, feed.Name, feed.URI)
+
+	// Retrieve the data to search.
+	document, err := m.retrieve(feed)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, channelItem := range document.Channel.Item {
+		// Check the title for the search term.
+		matched, err := regexp.MatchString(searchTerm, channelItem.Title)
+		if err != nil {
+			return nil, err
+		}
+
+		// If we found a match save the result.
+		if matched {
+			results = append(results, &search.Result{
+				Field:   "Title",
+				Content: channelItem.Title,
+			})
+		}
+
+		// Check the description for the search term.
+		matched, err = regexp.MatchString(searchTerm, channelItem.Description)
+		if err != nil {
+			return nil, err
+		}
+
+		// If we found a match save the result.
+		if matched {
+			results = append(results, &search.Result{
+				Field:   "Description",
+				Content: channelItem.Description,
+			})
+		}
+	}
+
+	return results, nil
+}
+
+
+// retrieve 发送HTTP Get 请求获取rss 数据源并解码
+func (m rssMatcher) retrieve(feed *search.Feed) (*rssDocument, error) {
+	if feed.URI == "" {
+		return nil, errors.New("No rss feed uri provided")
+	}
+
+	// 从网络获得rss 数据源文档
+	resp, err := http.Get(feed.URI)
+	if err != nil {
+		return nil, err
+	}
+
+	// 一旦从函数返回，关闭返回的响应链接
+	defer resp.Body.Close()
+
+	// 检查状态码是不是200，这样就能知道
+	// 是不是收到了正确的响应
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("HTTP Response Error %d\n", resp.StatusCode)
+	}
+
+	// 将rss 数据源文档解码到我们定义的结构类型里
+	// 不需要检查错误，调用者会做这件事
+	var document rssDocument
+	err = xml.NewDecoder(resp.Body).Decode(&document)
+	return &document, err
+}
